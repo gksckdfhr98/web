9ab555f3849c3d117758a2bc289c9ad65fedc6b8
Han chang rok <gksckdfhr99@naver.com>
2020-10-21 16:25:41 +0900

    add function.js

---
+++ Back-end/Node.js/function.js
@@ -0,0 +1,71 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+function templateHTML(title,list,body){
+  return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + '</ul>';
+  return list;
+}
+
+var app = http.createServer(function(request,response){
+  var _url = request.url;
+  //url parsing
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+
+  if(pathname === '/'){
+    if(queryData.id === undefined){
+      fs.readdir('./data',function(err,filelist){
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var list = templateList(filelist);
+        var template = templateHTML(title,list,
+          `<h2>${title}</h2><p>${description}</p>`);
+          response.writeHead(200);
+          response.end(template);
+      });
+    }
+    else{
+      fs.readdir('./data',function(err,filelist){
+        fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title,list,
+            `<h2>${title}</h2><p>${description}</p>`);
+            response.writeHead(200);
+            response.end(template);
+        });
+      });
+    }
+  }
+  else{
+    response.writeHead(404);
+    response.end('Not Found');
+  }
+});
+
+//port num 3000으로 server start
+app.listen(3000);
