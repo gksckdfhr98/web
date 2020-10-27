ece4d38625c8bc150327fb94168f62647daf860e
gksckdfhr98 <56072258+gksckdfhr98@users.noreply.github.com>
2020-10-27 16:23:12 +0900

    Update container_with_most_water.cpp

--- Algorithm/leetcode/container_with_most_water.cpp
+++ Algorithm/leetcode/container_with_most_water.cpp
@@ -13,3 +13,18 @@ public:
     }
 };
 
+//two pointer
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxarea(0),i(0),j(height.size()-1);
+        while(i<j){
+            maxarea = max(maxarea, min(height[i],height[j])*(j-i));
+            if(height[i]<height[j]) i++;
+            else j--;
+        }
+        return maxarea;
+    }
+};
+
