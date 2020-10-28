376cedb969984b9459a061e4ba52f58773c92008
gksckdfhr98 <56072258+gksckdfhr98@users.noreply.github.com>
2020-10-29 01:19:43 +0900

    Update Search in Rotated Sorted Array.cpp

--- Algorithm/leetcode/Search in Rotated Sorted Array.cpp
+++ Algorithm/leetcode/Search in Rotated Sorted Array.cpp
@@ -10,3 +10,32 @@ public:
         return ans;
     }
 };
+
+//binary search
+
+class Solution {
+public:
+    int binarysearch(vector<int> &nums,int low,int high,int target){
+        if(high<low) return -1;
+        
+        int mid = low + (high-low)/2;
+        
+        if(nums[mid]==target) return mid;
+        
+        if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && target<=nums[mid])
+                return binarysearch(nums,low,mid-1,target);
+            return binarysearch(nums,mid+1,high,target);
+        }
+        // low에서 mid 사이는 오름차순이 아님.
+        if(nums[mid]<=target && target<=nums[high])
+            return binarysearch(nums,mid+1,high,target);
+        return binarysearch(nums,low,mid-1,target);
+    }
+    
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int ans = binarysearch(nums,0,n-1,target);
+        return ans;
+    }
+};
