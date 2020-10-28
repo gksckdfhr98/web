c4d073ba62517719f1938122bd1f06d1f3bf9b14
gksckdfhr98 <56072258+gksckdfhr98@users.noreply.github.com>
2020-10-28 20:23:56 +0900

    Update Swap Nodes in Pairs.cpp

--- Algorithm/leetcode/Swap Nodes in Pairs.cpp
+++ Algorithm/leetcode/Swap Nodes in Pairs.cpp
@@ -22,4 +22,24 @@ public:
     }
 };
 
-//
+//iteration
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *prev = dummy;
+        while(prev->next && prev->next->next){
+            ListNode *first = prev->next, *second = prev->next->next;
+            first->next = second->next;
+            second->next = first;
+            prev->next = second;
+            prev = first;
+        }
+        
+        return dummy->next;
+    }
+};
