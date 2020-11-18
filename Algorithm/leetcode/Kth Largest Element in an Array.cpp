63cdb19c53cc13cc54529fc78c49f28276769c3a
gksckdfhr98 <56072258+gksckdfhr98@users.noreply.github.com>
2020-11-18 23:31:48 +0900

    Update Kth Largest Element in an Array.cpp

--- Algorithm/leetcode/Kth Largest Element in an Array.cpp
+++ Algorithm/leetcode/Kth Largest Element in an Array.cpp
@@ -6,3 +6,14 @@ public:
         return nums[n-k];
     }
 };
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(),nums.end());
+        
+        for(int i=0;i<k-1;i++) pq.pop();
+        
+        return pq.top();
+    }
+};
