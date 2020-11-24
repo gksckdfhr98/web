e2d4ed67712f3d012f1b6aae692fa6042777b175
gksckdfhr98 <56072258+gksckdfhr98@users.noreply.github.com>
2020-11-24 18:39:27 +0900

    Create Subarray Sum Equals K.cpp

---
+++ Algorithm/leetcode/Subarray Sum Equals K.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int sum=0,ans=0;
+        map<int,int> m;
+        m[0]++;
+        
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(m.find(sum-k)!=m.end()){
+                ans+=m[sum-k];
+            }
+            m[sum]++;
+        }
+        return ans;
+    }
+};
